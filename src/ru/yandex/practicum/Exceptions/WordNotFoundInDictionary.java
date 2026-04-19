39079a0c07f2eeee088e7dd6e9b8cc93b37b96e1
Radmir Nurgaliev <nradmir@gmail.com>
2026-04-19 14:03:51 +0300

    fix: исправлены ошибки аудита

--- src/ru/yandex/practicum/Exceptions/WordNotFoundInDictionary.java
+++ src/ru/yandex/practicum/Exceptions/WordNotFoundInDictionary.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.Exceptions;
 
-import java.io.PrintWriter;
-
 public class WordNotFoundInDictionary extends Exception {
     public WordNotFoundInDictionary() {
         super("Данное слово отсутствует в словаре.");
--- src/ru/yandex/practicum/Exceptions/WordNullOrIncorrectLength.java
+++ src/ru/yandex/practicum/Exceptions/WordNullOrIncorrectLength.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.Exceptions;
 
-import java.io.PrintWriter;
-
 public class WordNullOrIncorrectLength extends Exception {
     public WordNullOrIncorrectLength(String message) {
         super(message);
